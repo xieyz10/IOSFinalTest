cf1c31ad7fe2f2b883f959bfa04d4f8642029e7e
xieyz10 <54917731+xieyz10@users.noreply.github.com>
2022-12-08 15:39:06 -0500

    first commit

---
+++ FinalTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalTest
+//
+//  Created by Mingyuan Xie on 12/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
